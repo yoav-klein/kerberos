04179072ee8a6b01f0517ac25c98f8ecca351d4b
Yoav Klein <yoavklein25@gmail.com>
2022-02-02 08:24:03 +0200

    copy-binaries fixed

--- scripts/copy-binaries.sh
+++ scripts/copy-binaries.sh
@@ -7,5 +7,5 @@ echo -e "\e[32;1m=== Copying applications to containers\e[0m"
 client_container=krb5-client
 server_container=krb5-server
 
-docker cp bin/server ${server_container}:/server
-docker cp bin/client ${client_container}:/client
+docker cp bin/server/. ${server_container}:/server
+docker cp bin/client/. ${client_container}:/client
